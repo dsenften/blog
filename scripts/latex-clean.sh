a1e276f951f99512b80ff7604f9fbb85a42d7f0f
Daniel Senften <daniel@senften.org>
2019-01-15 10:48:26 +0100

    Aufsetzen des Projektes mit allen notwendigen Dateien.

---
+++ scripts/latex-clean.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+rm *.aux
+rm *.loe
+rm *.log
+rm *.lol
+rm *.nav
+rm *.out
+rm *.pyg
+rm *.snm
+rm *.toc
+rm *.vrb
+
+rm -rf _minted*
+
+rm main.article.pdf
+rm main.beamer.pdf
